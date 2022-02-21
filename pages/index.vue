c3d864f3bab50d44f4824a038d949a011e549030
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-02-21 09:52:47 +0900

    storeをmodulesモードへ移動

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="container">
     <div>
-      <p>{{ $store.state.message }}</p>
+      <p>{{ $store.state.hello.message }}</p>
+      <!-- moduresモードで値を表示するには値の前にファイル名を記載 -->
+
       <!-- <button v-on:click="$store.commit('updateMessage')">Update</button> -->
       <!-- mutationsの機能をstore側から呼び出すには、$store.commitを使用しないといけない -->
       <!-- <button v-on:click="$store.commit('updateMessage','Commit with payload')">Update</button> -->
@@ -9,9 +11,12 @@
       <!-- 値を渡したいときは第二引数に値を入れるこれをpayloadという -->
       <!-- <button v-on:click="$store.dispatch('updateMessageActions')">Update</button> -->
       <!-- storeのactionをよぶにはdispatchを使う -->
-      <button v-on:click="$store.dispatch('updateMessageActions','Commit with payload')">Update</button>
+      <!-- <button v-on:click="$store.dispatch('updateMessageActions','Commit with payload')">Update</button> -->
       <!-- storeのactionをよぶにはdispatchメソッドを使う -->
       <!-- 値を渡したいときは第二引数に値を入れるこれをpayloadという -->
+
+      <button v-on:click="$store.dispatch('hello/updateMessageActions','Commit with payload')">Update</button>
+      <!-- アクションの前にファイル名を記載 -->
     </div>
   </section>
 </template>
